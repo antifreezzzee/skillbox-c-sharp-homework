4cbeda0271020031168224533906888b1f33b962
DESKTOP-H4KQ5I5\Serega <spinnaker.studio@gmail.com>
2022-02-21 15:33:13 +0300

    Home Work 5.5

---
+++ Theme_05/HomeWork_Task_1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace HomeWork_Task_1
+
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Просим пользователя ввести предложение
+            Console.WriteLine("Введите предложение:");
+            
+            //Считываем ввод и заносим в переменную
+            String text = Console.ReadLine();
+            Console.WriteLine("\nРазделяем его на слова...");
+            
+            //Вызываем метод splitText и сохраняем результат его работы в массив строк.
+            String[] strings = splitText(text);
+            
+            //Вызываем метод printResult и передаем в него полученный массив.
+            printResult(strings);
+
+        }
+
+        //Данный метод принимает строку и возвращает массив подстрок.
+        //Метод Split разбивает исходную строку на слова, разделенные символом, переданным в аргументе.
+        //В нашем случае это пробел.
+        private static String[] splitText(String text)
+        {
+            return text.Split(' ');
+        }
+
+        //Данный метод выполняет построчный вывод содержимого переданного масива.
+        private static void printResult(String[] Strings)
+        {
+            Console.WriteLine("\nВот что получилось:\n");
+            foreach (var VARIABLE in Strings)
+            {
+                Console.WriteLine(VARIABLE);
+            }
+        }
+    }
+}
---
+++ Theme_05/HomeWork_Task_2/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace HomeWork_Task_2
+
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Просим пользователя ввести предложение
+            Console.WriteLine("Введите предложение:");
+            
+            //Считываем ввод и заносим в переменную
+            String text = Console.ReadLine();
+            Console.WriteLine("\nИнвертируем предложение...");
+            
+            //Выыодим значение, полученное в результате инвертирования слов впредложении
+            Console.WriteLine($"Вот что получилось:\n{ReverseWords(text)}");
+            
+        }
+
+        //Данный метод принимает аргументом строку. С помощью метода splitText делит её на слова,
+        //затем склеивает их в обратном порядке, формируя и возвращая новую строку. 
+        private static String ReverseWords(String text)
+        {
+            String[] stringsUntil = splitText(text);
+            String stringsAfter = "";
+            for (int i = stringsUntil.Length - 1; i >= 0; i--)
+            {
+                stringsAfter += stringsUntil[i] + " ";
+            }
+            return stringsAfter;
+        }
+
+        //Данный метод принимает строку и возвращает массив подстрок.
+        //Метод Split разбивает исходную строку на слова, разделенные символом, переданным в аргументе.
+        //В нашем случае это пробел.
+        private static String[] splitText(String text)
+        {
+            return text.Split(' ');
+        }
+    }
+}
